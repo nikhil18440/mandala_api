6ebf08e60524e06dc1ff272909642d7a17a6fafb
nikhil <nickjr1032@gmail.com>
2024-07-03 16:51:03 +0530

    hjghjg

--- client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const createProxyMiddleware = require('http-proxy-middleware')
-
-module.exports = function(app){
-    app.use("/api/**", createProxyMiddleware.createProxyMiddleware({
-        target: "https://mandala-api.vercel.app/",
-        secure: false,
-        changeOrigin: true
-    }))
-}
